826a49cc9b1dcabb983cf1748604ef7be3dc77df
Diego Quintanilla <diego.quintanilla@proton.me>
2024-04-18 20:43:01 -0400

    begin work on DB models file

--- back-end/api_v1/models.py
+++ back-end/api_v1/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+# User Model
+class User(models.Model):
+    username = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+    company_name = models.CharField(max_length=255, blank=True, null=True)
+    role = models.CharField(max_length=255, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
