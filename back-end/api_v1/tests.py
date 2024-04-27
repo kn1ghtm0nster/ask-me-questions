43b35a416bb98397723f279c8ec1970a31b79c00
Diego Quintanilla <diego.quintanilla@proton.me>
2024-04-27 18:33:05 -0400

    [CI] Add GitHub Actions workflow for running tests on push events

--- back-end/api_v1/tests.py
+++ back-end/api_v1/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+# TODO: REMOVE THE CODE BELOW. THIS IS DUMMY CODE
+
+
+def add_numbers(num1, num2) -> int or None:
+    if type(num1) is int and type(num2) is int:
+        return num1 + num2
+    return None
+
+
+class TestAddNumbers(TestCase):
+    def test_add_numbers(self):
+        self.assertEqual(add_numbers(1, 2), 3)
+        self.assertEqual(add_numbers(0, 0), 0)
+        self.assertEqual(add_numbers(-1, 1), 0)
+        self.assertEqual(add_numbers(-1, -1), -2)
+        self.assertEqual(add_numbers(1, -1), 0)
+        self.assertEqual(add_numbers(1.0, 1), None)
+        self.assertEqual(add_numbers(1, 1.0), None)
+        self.assertEqual(add_numbers(1.0, 1.0), None)
