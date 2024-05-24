109beedc8af04c6eccadc0390001d25e4957b208
Diego Quintanilla <diego.quintanilla@proton.me>
2024-05-23 22:01:10 -0400

    update to settings.py for .env usage

--- back-end/ask_me_questions/settings.py
+++ back-end/ask_me_questions/settings.py
@@ -25,6 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
+# SECURITY WARNING: keep password to your database profile secret!
+POSTGRES_PASSWORD = config('PASSWORD')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -83,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'ama_db',
         'USER': 'diego_quintanilla',
-        'PASSWORD': '',
+        'PASSWORD': POSTGRES_PASSWORD,
         'HOST': 'localhost',
         'PORT': '',
     }
